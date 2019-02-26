0cd91b68836e6197e95fa30d31561b3eabd78ad8
onrolll <controlonrolll@gmail.com>
2019-02-27 00:54:56 +0200

    initial aggregation test running ok

--- kafka-balance-stream/src/test/kotlin/kafka/balance/stream/BalanceStreamTest.kt
+++ kafka-balance-stream/src/test/kotlin/kafka/balance/stream/BalanceStreamTest.kt
@@ -1,7 +1,5 @@
 package kafka.balance.stream
 
-
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.*
 import kotlin.test.Test
 import org.apache.kafka.streams.StreamsBuilder
@@ -12,12 +10,16 @@ import org.apache.kafka.streams.StreamsConfig
 import org.apache.kafka.streams.TopologyTestDriver
 import org.apache.kafka.streams.test.ConsumerRecordFactory
 import org.apache.kafka.streams.test.OutputVerifier
-
+import org.junit.After
+import org.junit.Before
 
 class BalanceStreamTest {
 
-    @Test
-    fun testAggregation() {
+    private var testDriver: TopologyTestDriver? = null
+    private val factory = ConsumerRecordFactory<Int, Int>("input-topic",IntegerSerializer(), IntegerSerializer())
+
+    @Before
+    fun setup() {
         val builder = StreamsBuilder();
         builder.stream<Int, Int>("input-topic")
                 .groupBy{ address, balance -> address}
@@ -26,23 +28,32 @@ class BalanceStreamTest {
                         { address, balance, previousBalance ->  balance + previousBalance },
                         Materialized.`as`("balance")
 
-                ).to("output-topic");
+                ).toStream().to("output-topic");
         val topology = builder.build();
         val config = Properties().apply {
             setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "test")
             setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234")
+            setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().javaClass.name)
+            setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer().javaClass.name)
 
         }
-        var testDriver = TopologyTestDriver(topology, config)
-        val factory = ConsumerRecordFactory<Int, Int>("input-topic",IntegerSerializer(), IntegerSerializer())
-        testDriver.pipeInput(factory.create( Integer.valueOf(101), Integer.valueOf(101)))
-        testDriver.pipeInput(factory.create( Integer.valueOf(101), Integer.valueOf(101)))
-        testDriver.pipeInput(factory.create( Integer.valueOf(101), Integer.valueOf(101)))
-        val outputRecord = testDriver.readOutput("output-topic", IntegerDeserializer(), IntegerDeserializer())
-        OutputVerifier.compareKeyValue(outputRecord, 101, 303)
-        testDriver.close();
+        testDriver = TopologyTestDriver(topology, config)
+    }
+    @After
+    fun tearDown() {
+        testDriver?.close()
     }
 
+
+    @Test
+    fun testAggregation() {
+        testDriver?.pipeInput(factory.create( Integer.valueOf(104), Integer.valueOf(101)))
+        val outputRecord1 = testDriver?.readOutput("output-topic", IntegerDeserializer(), IntegerDeserializer())
+        testDriver?.pipeInput(factory.create( Integer.valueOf(104), Integer.valueOf(101)))
+        val outputRecord2 = testDriver?.readOutput("output-topic", IntegerDeserializer(), IntegerDeserializer())
+        OutputVerifier.compareKeyValue(outputRecord1, 104, 101)
+        OutputVerifier.compareKeyValue(outputRecord2, 104, 202)
+    }
 
     private fun sum(balance: String, previousBalance: String): String {
         var result = BigInteger(balance) + BigInteger(previousBalance)
