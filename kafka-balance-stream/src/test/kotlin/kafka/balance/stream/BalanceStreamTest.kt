4abc1a8dc6b5ce917684d22e90c555684d305864
Stefan Vadev <stefan.vadev@gmail.com>
2019-02-26 10:56:37 +0200

    fix-serialization-issue

--- kafka-balance-stream/src/test/kotlin/kafka/balance/stream/BalanceStreamTest.kt
+++ kafka-balance-stream/src/test/kotlin/kafka/balance/stream/BalanceStreamTest.kt
@@ -1,17 +1,18 @@
 package kafka.balance.stream
 
 
-import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.serialization.*
-import kotlin.test.Test
+import org.apache.kafka.common.serialization.IntegerDeserializer
+import org.apache.kafka.common.serialization.IntegerSerializer
+import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.StreamsBuilder
-import org.apache.kafka.streams.kstream.Materialized
-import java.math.BigInteger
-import java.util.*
 import org.apache.kafka.streams.StreamsConfig
 import org.apache.kafka.streams.TopologyTestDriver
+import org.apache.kafka.streams.kstream.Materialized
 import org.apache.kafka.streams.test.ConsumerRecordFactory
 import org.apache.kafka.streams.test.OutputVerifier
+import java.math.BigInteger
+import java.util.*
+import kotlin.test.Test
 
 
 class BalanceStreamTest {
@@ -26,18 +27,22 @@ class BalanceStreamTest {
                         { address, balance, previousBalance ->  balance + previousBalance },
                         Materialized.`as`("balance")
 
-                ).to("output-topic");
+                ) to "output-topic"
         val topology = builder.build();
-        val config = Properties().apply {
-            setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "test")
-            setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234")
 
-        }
-        var testDriver = TopologyTestDriver(topology, config)
+        val props = Properties()
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().javaClass.getName())
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Integer().javaClass.getName())
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test")
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234")
+
+        val testDriver = TopologyTestDriver(topology, props)
         val factory = ConsumerRecordFactory<Int, Int>("input-topic",IntegerSerializer(), IntegerSerializer())
+
         testDriver.pipeInput(factory.create( Integer.valueOf(101), Integer.valueOf(101)))
         testDriver.pipeInput(factory.create( Integer.valueOf(101), Integer.valueOf(101)))
         testDriver.pipeInput(factory.create( Integer.valueOf(101), Integer.valueOf(101)))
+
         val outputRecord = testDriver.readOutput("output-topic", IntegerDeserializer(), IntegerDeserializer())
         OutputVerifier.compareKeyValue(outputRecord, 101, 303)
         testDriver.close();
