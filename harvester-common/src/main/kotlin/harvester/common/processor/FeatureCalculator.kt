27f2c8c90567d84aab67d726cffa97d19bb821eb
StefanVadev <stefan.vadev@gmail.com>
2019-03-26 16:36:11 +0200

    use better type for big numbers + add sum function

--- harvester-common/src/main/kotlin/harvester/common/processor/FeatureCalculator.kt
+++ harvester-common/src/main/kotlin/harvester/common/processor/FeatureCalculator.kt
@@ -1,13 +1,25 @@
 package harvester.common.processor
 
+import java.math.BigInteger
+
 class FeatureCalculator {
     companion object {
-        fun increaseByOne(featureNumber: String): String {
-            return (featureNumber.toInt() + 1).toString()
+
+        fun sum(feature: String, previousFeature: String): String {
+            val result = BigInteger(feature).add(BigInteger(previousFeature))
+            // uncomment when starting from block 0
+//          if(result < BigInteger.ZERO){
+//            result = BigInteger.ZERO
+//          }
+            return result.toString()
         }
 
-        fun subtract(previousFeatureNumber: String, featureNumber: String): String {
-            return (previousFeatureNumber.toInt() - featureNumber.toInt()).toString()
+        fun increaseByOne(feature: String): String {
+            return BigInteger(feature).add(BigInteger.ONE).toString()
+        }
+
+        fun subtract(previousFeature: String, feature: String): String {
+            return BigInteger(previousFeature).subtract(BigInteger(feature)).toString()
         }
     }
 }
