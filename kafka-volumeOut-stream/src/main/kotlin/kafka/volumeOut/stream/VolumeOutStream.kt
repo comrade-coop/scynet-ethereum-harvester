a22418b4636bce016c13809d1ed803a1f9e739a1
StefanVadev <stefan.vadev@gmail.com>
2019-03-31 14:44:13 +0300

    lil tweak

--- kafka-volumeOut-stream/src/main/kotlin/kafka/volumeOut/stream/VolumeOutStream.kt
+++ kafka-volumeOut-stream/src/main/kotlin/kafka/volumeOut/stream/VolumeOutStream.kt
@@ -6,7 +6,7 @@ import kafka.volumeOut.stream.processor.VolumeOutProcessorSupplier
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.streams.*
 
-fun main(args: Array<String>) {
+fun main() {
     VolumeOutStream().start()
 }
 
