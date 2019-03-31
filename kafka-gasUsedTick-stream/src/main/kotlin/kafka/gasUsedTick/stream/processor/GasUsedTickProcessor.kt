1d5b6ad928afa9b1d158551bf0ccca63c68cc00d
StefanVadev <stefan.vadev@gmail.com>
2019-03-31 17:16:09 +0300

    use updated lib version

--- kafka-gasUsedTick-stream/src/main/kotlin/kafka/gasUsedTick/stream/processor/GasUsedTickProcessor.kt
+++ kafka-gasUsedTick-stream/src/main/kotlin/kafka/gasUsedTick/stream/processor/GasUsedTickProcessor.kt
@@ -2,7 +2,6 @@ package kafka.gasUsedTick.stream.processor
 
 import harvester.common.messages.Messages
 import harvester.common.processor.BlockFeatureTickProcessor
-import harvester.common.processor.FeatureCalculator
 
 class GasUsedTickProcessor : BlockFeatureTickProcessor("gasUsed") {
 
@@ -11,18 +10,6 @@ class GasUsedTickProcessor : BlockFeatureTickProcessor("gasUsed") {
 
         addToBlockNumberFeatureStore(gasUsed)
         addToFeatureStore(gasUsed)
-    }
-
-    private fun addToBlockNumberFeatureStore(gasUsed: String) {
-        val builder = blockNumberFeatureStore!!.get(currentBlockNumber).toBuilder()
-        builder.putAddressFeature(PROPERTY, gasUsed)
-        blockNumberFeatureStore!!.put(currentBlockNumber, builder.build())
-    }
-
-    private fun addToFeatureStore(gasUsed: String) {
-        val oldValue = featureStore!!.get(PROPERTY)
-        println(oldValue)
-        featureStore!!.put(PROPERTY, FeatureCalculator.sum(oldValue, gasUsed))
     }
 
 }
