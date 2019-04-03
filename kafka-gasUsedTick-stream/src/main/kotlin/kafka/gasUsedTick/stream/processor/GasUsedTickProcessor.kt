b50793dc30d52aa7d0e29b084c1eeafe6d1bdccc
StefanVadev <stefan.vadev@gmail.com>
2019-04-03 18:54:56 +0300

    average gas used by tick

--- kafka-gasUsedTick-stream/src/main/kotlin/kafka/gasUsedTick/stream/processor/GasUsedTickProcessor.kt
+++ kafka-gasUsedTick-stream/src/main/kotlin/kafka/gasUsedTick/stream/processor/GasUsedTickProcessor.kt
@@ -2,14 +2,14 @@ package kafka.gasUsedTick.stream.processor
 
 import harvester.common.messages.Messages
 import harvester.common.processor.BlockFeatureTickProcessor
+import harvester.common.processor.FeatureCalculationStrategy
 
-class GasUsedTickProcessor : BlockFeatureTickProcessor("gasUsed") {
+class GasUsedTickProcessor : BlockFeatureTickProcessor("gasUsed", FeatureCalculationStrategy.AVERAGE) {
 
     override fun extract(block: Messages.Block) {
         val gasUsed = block.gasUsed
 
-        addToBlockNumberFeatureStore(gasUsed)
-        addToFeatureStore(gasUsed)
+        updateStores(gasUsed)
     }
 
 }
