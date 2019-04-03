010fb0840a97c58319a4b24205e6b82c72d4a390
StefanVadev <stefan.vadev@gmail.com>
2019-04-03 18:48:36 +0300

    block size average by tick

--- kafka-blockSizeTick-stream/src/main/kotlin/kafka/blockSizeTick/stream/processor/BlockSizeTickProcessor.kt
+++ kafka-blockSizeTick-stream/src/main/kotlin/kafka/blockSizeTick/stream/processor/BlockSizeTickProcessor.kt
@@ -2,14 +2,14 @@ package kafka.blockSizeTick.stream.processor
 
 import harvester.common.messages.Messages
 import harvester.common.processor.BlockFeatureTickProcessor
+import harvester.common.processor.FeatureCalculationStrategy
 
-class BlockSizeTickProcessor : BlockFeatureTickProcessor("blockSize") {
+class BlockSizeTickProcessor : BlockFeatureTickProcessor("blockSize", FeatureCalculationStrategy.AVERAGE) {
 
     override fun extract(block: Messages.Block) {
         val blockSize = block.size
 
-        addToBlockNumberFeatureStore(blockSize)
-        addToFeatureStore(blockSize)
+        updateStores(blockSize)
     }
 
 }
--- kafka-blockSizeTick-stream/src/main/kotlin/kafka/blockSizeTick/stream/processor/BlockSizeTickProcessorSupplier.kt
+++ kafka-blockSizeTick-stream/src/main/kotlin/kafka/blockSizeTick/stream/processor/BlockSizeTickProcessorSupplier.kt
@@ -4,7 +4,7 @@ import harvester.common.messages.Messages
 import org.apache.kafka.streams.processor.ProcessorSupplier
 import org.apache.kafka.streams.processor.Processor
 
-class BlockSizeTickProcessorSupplier: ProcessorSupplier<String, Messages.Block> {
+class BlockSizeTickProcessorSupplier : ProcessorSupplier<String, Messages.Block> {
     override fun get(): Processor<String, Messages.Block> {
         return BlockSizeTickProcessor()
     }
