525927f8fb19c693bf28519719c2273107ebd930
StefanVadev <stefan.vadev@gmail.com>
2019-04-04 17:24:20 +0300

    add common files used by distros

---
+++ harvester-common/src/main/kotlin/harvester/common/serialization/BlobDeserializer.kt
@@ -0,0 +1,16 @@
+package harvester.common.serialization
+
+import kafka.balanceLastSeen.distribution.messages.MatrixBlob
+import org.apache.kafka.common.serialization.Deserializer
+
+class BlobDeserializer: Deserializer<MatrixBlob.Blob> {
+    override fun configure(configs: MutableMap<String, *>?, isKey: Boolean) {
+    }
+
+    override fun deserialize(topic: String?, data: ByteArray?): MatrixBlob.Blob {
+        return MatrixBlob.Blob.parseFrom(data)
+    }
+
+    override fun close() {
+    }
+}
---
+++ harvester-common/src/main/kotlin/harvester/common/serialization/JoinSerializer.kt
@@ -0,0 +1,16 @@
+package harvester.common.serialization
+
+import kafka.balanceLastSeen.distribution.messages.StreamJoin
+import org.apache.kafka.common.serialization.Serializer
+
+class JoinSerializer: Serializer<StreamJoin.Join> {
+    override fun configure(configs: MutableMap<String, *>?, isKey: Boolean) {
+    }
+
+    override fun serialize(topic: String?, data: StreamJoin.Join?): ByteArray {
+       return data!!.toByteArray()
+    }
+
+    override fun close() {
+    }
+}
