c3a1cffe44976dc5ae90a141e6f59fbb743ae7ad
StefanVadev <stefan.vadev@gmail.com>
2019-04-05 11:40:12 +0300

    use common block feature processor

--- kafka-difficulty-stream/src/main/kotlin/kafka/difficulty/stream/processor/DifficultyProcessor.kt
+++ kafka-difficulty-stream/src/main/kotlin/kafka/difficulty/stream/processor/DifficultyProcessor.kt
@@ -1,30 +1,11 @@
 package kafka.difficulty.stream.processor
 
 import harvester.common.messages.Messages
-import org.apache.kafka.streams.processor.Processor
-import org.apache.kafka.streams.processor.ProcessorContext
+import harvester.common.processor.BlockFeatureProcessor
 
-class DifficultyProcessor : Processor<String, Messages.Block> {
-
-    private var context: ProcessorContext? = null
-
-    override fun process(blockNumber: String, block: Messages.Block) {
-        try {
-            process(block)
-        } catch (e: Exception) {
-            // TODO use logger for this
-            println("Exception occurred: $e while processing block: $blockNumber")
-        }
+class DifficultyProcessor : BlockFeatureProcessor() {
+    override fun getFeatureValue(block: Messages.Block): String {
+        return block.difficulty
     }
 
-    private fun process(block: Messages.Block) {
-        context!!.forward(block.number, block.difficulty)
-        context!!.commit()
-    }
-
-    override fun init(context: ProcessorContext?) {
-        this.context = context
-    }
-
-    override fun close() {}
 }
