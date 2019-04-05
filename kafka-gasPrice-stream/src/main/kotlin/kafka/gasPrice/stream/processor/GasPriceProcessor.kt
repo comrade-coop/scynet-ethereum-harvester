64da80e392c125702b7304c638af827850940af8
StefanVadev <stefan.vadev@gmail.com>
2019-04-05 11:42:13 +0300

    use block feature processor

--- kafka-gasPrice-stream/src/main/kotlin/kafka/gasPrice/stream/processor/GasPriceProcessor.kt
+++ kafka-gasPrice-stream/src/main/kotlin/kafka/gasPrice/stream/processor/GasPriceProcessor.kt
@@ -1,29 +1,12 @@
 package kafka.gasPrice.stream.processor
 
 import harvester.common.messages.Messages
-import org.apache.kafka.streams.processor.Processor
-import org.apache.kafka.streams.processor.ProcessorContext
+import harvester.common.processor.BlockFeatureProcessor
 import java.math.BigDecimal
 
-class GasPriceProcessor : Processor<String, Messages.Block> {
-
-    private var context: ProcessorContext? = null
-
-    override fun process(blockNumber: String, block: Messages.Block) {
-        try {
-            process(block)
-        } catch (e: Exception) {
-            // TODO use logger for this
-            println("Exception occurred: $e while processing block: $blockNumber")
-        }
-    }
-
-    private fun process(block: Messages.Block) {
-        val averageGasPrice = averageGasPrice(block)
-        val blockNumber = block.number
-
-        context!!.forward(blockNumber, averageGasPrice)
-        context!!.commit()
+class GasPriceProcessor : BlockFeatureProcessor() {
+    override fun getFeatureValue(block: Messages.Block): String {
+        return averageGasPrice(block)
     }
 
     private fun averageGasPrice(block: Messages.Block): String {
@@ -40,9 +23,4 @@ class GasPriceProcessor : Processor<String, Messages.Block> {
         return (totalGasPriceAmount / transactions.size.toBigDecimal()).toString()
     }
 
-    override fun init(context: ProcessorContext?) {
-        this.context = context
-    }
-
-    override fun close() {}
 }
